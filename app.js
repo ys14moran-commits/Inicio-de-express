1df46b94fe619195e4cc055c3fdcc081c51a0996
ys14moran-commits <ys14moran@gmail.com>
2026-07-24 23:49:56 -0500

    configuracion de bariable parte 2

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ res.send("Aprendiendo express,ficha 3407181 yahir santiago moran chiquiza");
 app.listen(port, function () {
 console.log( `Servidor en funcionamiento en el puerto: ${port}`);
 });
+
+import express from "express"
+const app = express();
+const port = process.env.PUERTO || 3000
+app.get("/", function(req, res) {
+res.send("Hola aprendiendo express, con la ficha 3407181, ADSO en el SENA");
+});
+app.listen(port, function() {
+    console.log(`Servidor en funcionamiento en el puerto ${port}`);
+});
