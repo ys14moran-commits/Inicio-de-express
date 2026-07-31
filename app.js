47f5da762397877aeb402152be810ca8ea255602
ys14moran-commits <ys14mora@gmail.com>
2026-07-31 09:46:08 -0500

    cambio a module

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const express = require('express');
+//const express = require("express")
+import express from 'express';
+
+//leer el archivo .env
+import {configDotenv} from "dotenv"
+configDotenv()
+
 const app = express();
-const port = 3000;
+const port = process.env.PUERTO || 5050;
+
 app.get("/", (_, res) => {
-res.send("Aprendiendo express,ficha 3407181");
+  res.send("Aprendiendo express, ficha 3407181, ADSO en el curso de desarrollo web el 31 de julio de 2026");
 });
+
 app.listen(port, () => {
-console.log( `Servidor en funcionamiento en el puerto: ${port}`);
+  console.log(`Servidor en funcionamiento en el puerto: ${port}`);
 });
