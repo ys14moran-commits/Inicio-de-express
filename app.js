23a2a6343df1e4ec5a5569628afe72b6fdb9ab17
ys14moran-commits <ys14mora@gmail.com>
2026-07-31 10:48:35 -0500

    cambios de rutas y bariantes

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ app.get("/", (_, res) => {
   res.send("Aprendiendo express, ficha 3407181, ADSO en el curso de desarrollo web el 31 de julio de 2026");
 });
 
+app.get ("/otraruta", (req, res)=>{
+    res.send(`<h1>Otra ruta ejemplo</h1>
+        <h2>End point con res.send</h2>`)
+});
+
+app.get ("/ruta2", (req, res)=>{
+    res.json({"nombre" : "yo", "apellido": "no", "cargo": "mucho"})
+});
+
 app.listen(port, () => {
-  console.log(`Servidor en funcionamiento en el puerto: ${port}`);
+  console.log(`Servidor en funcionamiento en el puerto: http://localhost:${port}`)
 });
 
