40fbb26af1c0400beec1af799ed1dc8341bf95c8
yahir santiago moran chiquiza <ys14moran@gmail.com>
2026-08-14 05:02:13 -0500

    creacion del archivo app.js, modificacion de pakage.json, prueva de funcion del servidor 3030

---
+++ intro-express-js-Yahir-santiago-Moran-Chiquiza/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+ const express = require('express');
+  const app = express();
+   const PORT = process.env.PORT || 3000;
+    // Middleware para parsear JSON en el cuerpo de las peticiones
+ app.use(express.json());
+  // Endpoint 1: Saludo estático
+   app.get('/saludo', (req, res) => { 
+    res.send('Hola mundo'); });
+     // Endpoint 2: Saludo dinámico con el nombre del aprendiz (Parámetro de ruta) 
+     app.get('/saludo/:nombre', (req, res) => { 
+        const { nombre } = req.params; 
+        res.json({ 
+            mensaje: `¡Hola, ${nombre}! Bienvenido/a al taller de Express.` 
+        }); 
+    }); 
+    // Iniciar el servidor
+ app.listen(PORT, () => { 
+    console.log(`Servidor corriendo exitosamente en el puerto http://localhost:3030/ ${PORT}`); }); 
