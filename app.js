b80ed297f9f3d328c78a7f5685f17e7b701573e6
yahir <ys14moran@gmail.com>
2026-09-11 05:47:12 -0500

    enpoit de creacion aprendiz con form y json utilizando middleware bodyparse

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require ("express");
 const app = express();
 require("dotenv/config")
 const puerto = process.env.PUERTO || 3000;
+//configurar para la lectura del archivo
 const sistemaArchivo = require("fs")
 const ruta = require("path")
 const rutaArchivoJson = ruta.join(__dirname, "datos.json")
+//importal libreria para subir archivos
+const multer = require("multer")
 
+//middlewa body-parse, formatea los datos enviados
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//enpoin raiz
 app.get("/", function(req, res){
-    res.send("Api - rest aprendices");
+    res.send("Hola aprendiz");
 });
 
 app.get("/api/aprendices", function(req, res){
@@ -20,12 +28,31 @@ app.get("/api/aprendices", function(req, res){
     })
 })
 
-//aprendices
-app.post("/api/aprendices",(req,res) =>{
-    res.json({Mensaje: "Trabajando en el enpoint"})
+
+//validar que se envien los datos
+app.post("/api/aprendices", function(req, res){
+    const nuevoAprendiz = req.body
+    //utilizamos la lectura del archivo
+    sistemaArchivo.readFile(rutaArchivoJson,"utf-8", (error, datos) =>{
+        if(error){
+            return res.json({Error: "no se puede leer los datos"})
+        }
+        const listaAprendices =JSON.parse(datos)
+        //agregar un nuevo aprendiz
+        listaAprendices.push(nuevoAprendiz)
+        //escribir en el archibo
+        sistemaArchivo.writeFile(rutaArchivoJson, JSON.stringify(listaAprendices, null, 2), (error) =>{
+            if(error){
+    return res.status(500).json({Error: "No se puede registrar el aprendiz"})
+                    }
+            res.status(201).json({Mensaje: "Aprendiz creado con exito"})
+        })
+    })
 })
 
+
+
 app.listen(puerto, function(){
-     console.log(`Servidor corriendo exitosamente en el puerto http://localhost:${puerto}`);
+    console.log(`Servidor corriendo exitosamente en el puerto http://localhost:${puerto}`);
     
 }); 
